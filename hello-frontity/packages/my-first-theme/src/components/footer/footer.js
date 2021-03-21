880c5056a19ee930e56efc985ff37d0d840c3633
Jamie Wright <wright_jamie@hotmail.com>
2021-03-21 10:02:56 +0000

    Copyright Component added and flex for Footer

--- hello-frontity/packages/my-first-theme/src/components/footer/footer.js
+++ hello-frontity/packages/my-first-theme/src/components/footer/footer.js
@@ -7,17 +7,20 @@ import Menu from "../header/menu";
 const Footer = ({ state }) => {
 
     return (
+        <>
             <Div>
                 <FooterContainer>
-                    <SocialLinks />
-                    <Menu data={state.theme.menu} />
-                    <Menu data={state.theme.external} />
-                    <Menu data={state.theme.useful} />
-                    <div>
-                        RAF AC 2021
-                    </div>
+                    <FooterSocialLinks />
+                    <FooterMenu data={state.theme.menu} />
+                    <FooterMenu data={state.theme.external} />
+                    <FooterMenu data={state.theme.useful} />
+
                 </FooterContainer>
             </Div>
+            <Copyright>
+                RAF AC 2021
+            </Copyright>
+        </>
     )
 }
 
@@ -26,10 +29,33 @@ export default connect(Footer)
 const Div = styled.div`
     width:100%;
     background-color: var(--blue);
-    }
-`
+    `
+
 const FooterContainer = styled.footer`
     max-width: 800px;
     padding: .5em 1em;
     margin: auto;
+    display:flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    `
+const FooterSocialLinks = styled(SocialLinks)`
+    width: 100%;
+`
+
+const FooterMenu = styled(Menu)`
+    font-size: 1.5em;
+    flex: 0 1 auto;
+    
+  &:before{
+    content: "\00BB";
+    color: var(--accent);
+    font-size:1.2em;
+  }
+    `
+const Copyright = styled.div`
+    width:100%;
+    padding: 1em;
+    background: var(--light);
+    color: var(--text);
     `
